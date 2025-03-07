bca623b29e1b8c6660b1f4aecf8a6619a19f5201
Slavomir Grigorov <slavomir.grigorov@dreamix.eu>
2025-03-07 13:53:25 +0200

    System message added

--- src/main/java/eu/dreamix/ai/my_ai_playground/service/ChatService.java
+++ src/main/java/eu/dreamix/ai/my_ai_playground/service/ChatService.java
@@ -1,21 +1,34 @@
 package eu.dreamix.ai.my_ai_playground.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChatService {
 
     private final ChatClient chatClient;
+    private final Message systemMessage = new SystemMessage("You are a scientific chatbot, you speak very exactly. And you are very helpful.");
 
     public ChatService(ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
     public String generateTextSynchronous(String text){
+        List<Message> messages = new ArrayList<>();
+        messages.add(systemMessage);
+        messages.add(new UserMessage(text));
 
-        return chatClient.prompt(text).call().content();
-
+        Prompt prompt = new Prompt(messages);
+        List<Generation> results = chatClient.prompt(prompt).call().chatResponse().getResults();
+        return chatClient.prompt(prompt).call().content();
     }
 
 }
